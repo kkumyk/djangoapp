74607e5e027043503084c2e2cd7d2a208708f9f2
kkumyk <k.kumykova@gmail.com>
2024-05-08 15:37:58 +0100

    adjust indentation for the Meta class in Post model

--- blog/models.py
+++ blog/models.py
@@ -22,8 +22,8 @@ class Post(models.Model):
     class Meta:
         ordering = ["-created_on"]
 
-def __str__(self):
-    return f"{self.title} | written by {self.author}"
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
     
 
 class Comment(models.Model):

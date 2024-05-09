edeb0b057879e6618824ee6ef85ece9cd0f1d23f
kkumyk <k.kumykova@gmail.com>
2024-05-09 10:23:16 +0100

    import urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 # This file is where we'll list our blog app-specific URLs.
+
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+    path("", views.PostList.as_view(), name='home'),
 ]

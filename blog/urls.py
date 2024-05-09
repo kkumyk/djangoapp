3ca509ace7d487a19b9c3e48c858c9636dd7cdc7
kkumyk <k.kumykova@gmail.com>
2024-05-09 13:21:41 +0100

    testing passing data from a view to a template

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,8 @@
 # This file is where we'll list our blog app-specific URLs.
-
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name='home'),
-    path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.post_detail, name="post_detail"),
 ]

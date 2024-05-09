4d3ee806ac89e021b0878b3fc0296c929fadbb75
kkumyk <k.kumykova@gmail.com>
2024-05-09 10:06:55 +0100

    disply post generic views

---
+++ blog/urls.py
@@ -0,0 +1,7 @@
+# This file is where we'll list our blog app-specific URLs.
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+]
